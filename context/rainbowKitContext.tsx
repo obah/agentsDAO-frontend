24ef4b0df237e714dbbf9a688e173b88088d70a6
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-10-10 09:05:36 +0100

    feat: integrated frontend with dao smart contract

---
+++ context/rainbowKitContext.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig, sepolia } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import React from "react";
+
+const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "Agents Dao",
+  projectId: "852540ca8d1f1dc5d246d0fa982aac30",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export function RainbowKitContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
